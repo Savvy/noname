c591fda58f9c4e988324fe973a358f690facf78d
Savvy <malcomeqgreen@gmail.com>
2022-01-28 13:24:53 -0500

    Minor adjustments

--- components/account/details.vue
+++ components/account/details.vue
@@ -1,120 +1,131 @@
 <template>
-    <div class="account-details">
-        <div class="title">Account Details</div>
-        <form>
-            <div class="input-group align-center">
-                <span>Username:</span>
-                <div class="default-box">
-                    Cyber
-                    <div class="btn btn-default">Change</div>
-                </div>
-            </div>
-            <div class="input-group align-center">
-                <span>Email:</span>
-                <div class="default-box">
-                    user@example.com
-                    <div class="btn btn-default">Change</div>
-                </div>
-            </div>
-            <div class="input-group disclaimer">
-                <span>Avatar:</span>
-                <div class="change-avatar">
-                    <CommonAvatar src="https://i.imgur.com/rzuOBa8.png" borderRadius="5px" height="80px" width="80px" :pointer=true @click.native="$refs.changeAvatar.click()" />
-                    <input type="file" ref="changeAvatar" style="display: none" accept=".gif,.jpeg,.jpg,.png">
-                    <span>Click the image to change your avatar.</span>
-                </div>
-            </div>
-            <div class="input-group">
-                <span>Gender:</span>
-                <div class="input-group-radio">
-                    <input type="radio" name="gender" id="male" value="male" checked>
-                    <label for="male">Male</label>
-                    <input type="radio" name="gender" id="female" value="female">
-                    <label for="female">Female</label>
-                    <input type="radio" name="gender" id="other" value="other">
-                    <label for="other">Other</label>
-                </div>
-            </div>
-            <div class="input-group">
-                <span>About Me:</span>
-                <client-only>
-                    <CommonRichEditor btn-text="Post" />
-                </client-only>
-            </div>
-            <button type="submit" class="btn btn-primary align-self-center">Save</button>
-        </form>
-        <!--
+  <div class="account-details">
+    <div class="title">Account Details</div>
+    <form>
+      <div class="input-group align-center">
+        <span>Username:</span>
+        <div class="default-box">
+          Cyber
+          <div class="btn btn-default">Change</div>
+        </div>
+      </div>
+      <div class="input-group align-center">
+        <span>Email:</span>
+        <div class="default-box">
+          user@example.com
+          <div class="btn btn-default">Change</div>
+        </div>
+      </div>
+      <div class="input-group disclaimer">
+        <span>Avatar:</span>
+        <div class="change-avatar">
+          <CommonAvatar
+            src="https://i.imgur.com/rzuOBa8.png"
+            borderRadius="5px"
+            height="80px"
+            width="80px"
+            :pointer="true"
+            @click.native="$refs.changeAvatar.click()"
+          />
+          <input
+            type="file"
+            ref="changeAvatar"
+            style="display: none"
+            accept=".gif,.jpeg,.jpg,.png"
+          />
+          <span>Click the image to change your avatar.</span>
+        </div>
+      </div>
+      <div class="input-group">
+        <span>Gender:</span>
+        <div class="input-group-radio">
+          <input type="radio" name="gender" id="male" value="male" checked />
+          <label for="male">Male</label>
+          <input type="radio" name="gender" id="female" value="female" />
+          <label for="female">Female</label>
+          <input type="radio" name="gender" id="other" value="other" />
+          <label for="other">Other</label>
+        </div>
+      </div>
+      <div class="input-group">
+        <span>About Me:</span>
+        <client-only>
+          <CommonRichEditor btn-text="Save" />
+        </client-only>
+      </div>
+      <button type="submit" class="btn btn-primary align-self-center">
+        Save
+      </button>
+    </form>
+    <!--
             - Username,
             - Email,
             - Avatar,
             - Gender,
             - About Me,
         -->
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-        }
-    },
-    methods: {
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <style scoped>
-
 .title {
-    font-weight: 500;
-    font-size: 1.2rem;
-    margin-bottom: 15px;
+  font-weight: 500;
+  font-size: 1.2rem;
+  margin-bottom: 15px;
 }
 
 form {
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
 }
 
 .input-group {
-    flex: 1 0 auto;
-    max-width: initial;
-    display: flex;
-    flex-direction: row;
+  flex: 1 0 auto;
+  max-width: initial;
+  display: flex;
+  flex-direction: row;
 }
 
 .input-group > span {
-    min-width: 120px;
+  min-width: 120px;
 }
 
 .change-avatar span {
-    display: block;
-    margin-top: 5px;
-    font-size: 12px;
+  display: block;
+  margin-top: 5px;
+  font-size: 12px;
 }
 
 .default-box {
-    display: flex;
-    align-items: center;
-    gap: 10px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 
 label {
-    margin-right: 20px;
+  margin-right: 20px;
 }
 
-button[type='submit'] {
-    font-size: 16px;
-    min-width: 120px;
+button[type="submit"] {
+  font-size: 16px;
+  min-width: 120px;
 }
 
 .avatar {
-    transition: filter 0.25s ease-in-out;
+  transition: filter 0.25s ease-in-out;
 }
 
 .avatar:hover {
-    filter: grayscale(1);
+  filter: grayscale(1);
 }
 </style>
--- components/common/Category.vue
+++ components/common/Category.vue
@@ -1,77 +1,94 @@
 <template>
-    <div class="category d-flex align-center">
-        <div class="icon"><i class="bi bi-megaphone"></i></div>
-        <div class="category-info w-auto">
-            <nuxt-link to="/category" class="category-title">News & Announcements</nuxt-link>
-            <div class="category-meta">Topics: <span>481</span> Posts: <span>1028</span></div>
-        </div>
-        <div class="recent-thread d-flex flex-row">
-            <nuxt-link to="/profile">
-                <CommonAvatar src="https://i.imgur.com/45vM6qK.jpg" borderRadius="100px" height="40px" width="40px" :pointer=true />
-            </nuxt-link>
-            <div class="thread-info d-flex flex-column">
-                <nuxt-link to="/thread" class="thread-title">New API Changes</nuxt-link>
-                <div class="thread-meta">
-                    <nuxt-link to="/profile">Chroma</nuxt-link>
-                    <span class="sep"></span>
-                    <span class="time">3 days ago</span>
-                </div>
-            </div>
-        </div>
+  <div class="category d-flex align-center">
+    <div class="icon"><i class="bi bi-megaphone"></i></div>
+    <div class="category-info w-auto">
+      <nuxt-link to="/category" class="category-title"
+        >News & Announcements</nuxt-link
+      >
+      <div class="category-meta">
+        Topics: <span>481</span> Posts: <span>1028</span>
+      </div>
     </div>
+    <div class="recent-thread d-flex flex-row">
+      <nuxt-link to="/profile">
+        <CommonAvatar
+          src="https://i.imgur.com/45vM6qK.jpg"
+          borderRadius="100px"
+          height="40px"
+          width="40px"
+          :pointer="true"
+        />
+      </nuxt-link>
+      <div class="thread-info d-flex flex-column">
+        <nuxt-link to="/thread" class="thread-title">New API Changes</nuxt-link>
+        <div class="thread-meta">
+          <nuxt-link to="/profile">Chroma</nuxt-link>
+          <span class="sep"></span>
+          <span class="time">3 days ago</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .category {
-    padding: 15px 20px;
+  padding: 15px 20px;
 }
 
 .category > .icon {
-    margin-right: 15px;
-    font-size: 32px;
+  margin-right: 15px;
+  font-size: 32px;
 }
 
 .icon > i {
+  color: var(--primary-color);
+}
+
+@supports (-webkit-background-clip: text) or (background-clip: text) {
+  .icon > i {
     background: var(--primary-gradient);
     -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    color: transparent;
+  }
 }
 
 .category-title {
-    color: var(--light-text);
-    font-size: 1.15rem;
+  color: var(--light-text);
+  font-size: 1.15rem;
 }
 
 .category-meta {
-    font-size: 0.88rem;
-    color: var(--primary-color);
+  font-size: 0.88rem;
+  color: var(--primary-color);
 }
 
 .category-meta span {
-    font-weight: 600;
+  font-weight: 600;
 }
 
 .recent-thread {
-    gap: 12px;
+  gap: 12px;
 }
 
 .thread-info {
-    gap: 5px;
+  gap: 5px;
 }
 
 .thread-info > .thread-title {
-    font-weight: 500;
-    color: var(--light-text);
+  font-weight: 500;
+  color: var(--light-text);
 }
 
 .thread-meta {
-    font-size: 0.85rem;
-    display: flex;
-    align-items: center;
+  font-size: 0.85rem;
+  display: flex;
+  align-items: center;
 }
 
 .thread-meta > .time {
-    color: var(--primary-color);
-    font-weight: 300;
+  color: var(--primary-color);
+  font-weight: 300;
 }
 </style>
