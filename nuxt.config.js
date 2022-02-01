09ef38f96c9b055807182e84e49f2d7805d60d71
Savvy <malcomeqgreen@gmail.com>
2022-02-01 17:32:37 -0500

    Added process environment variable for backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,12 +47,16 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000/',
+    baseURL: process.env.BACK_HOST || 'http://localhost:5000/',
     credentials: true,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  env: {
+    BACK_HOST: process.env.BACK_HOST || 'http://localhost:5000/'
   },
 
   router: {
