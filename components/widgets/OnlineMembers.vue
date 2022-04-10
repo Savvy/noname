ec6bd33dd62f7202f4bbde659dc0ce31e35c9401
Malcom Green <malcomeqgreen@gmail.com>
2022-04-10 14:23:07 -0400

    Online members will be displayed

--- components/widgets/OnlineMembers.vue
+++ components/widgets/OnlineMembers.vue
@@ -1,20 +1,46 @@
 <template>
     <CommonSideWidget>
-        <template v-slot:widget-header>Online Members</template>
+        <template v-slot:widget-header>Online Members <span>({{ onlineUsers.length }})</span></template>
         <template v-slot:widget-body>
-            <div class="online-users d-flex">
-                <nuxt-link to="/profile/Malcom" v-for="index in 12" :key="index">
-                    <CommonAvatar src="https://i.imgur.com/45vM6qK.jpg" borderRadius="100px" height="40px" width="40px" 
+            <div class="online-users d-flex" v-if="onlineUsers.length > 0">
+                <nuxt-link :to="`/profile/${online.username}`" v-for="(online, index) in onlineUsers" :key="index">
+                    <CommonAvatar :src="online.details.avatar" borderRadius="100px" height="40px" width="40px" 
                     :pointer=true :shrinkOnHover=true />
                 </nuxt-link>
+            </div>
+            <div class="online-users d-flex" v-else>
+                <h5>No online users</h5>
             </div>
         </template>
     </CommonSideWidget>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            onlineUsers: {},
+        }
+    },
+    async mounted() {
+        let {data} = await this.$axios.get('/module/online');
+        this.onlineUsers = data.users;
+    },
+}
+</script>
+
 <style scoped>
+.widget-header span {
+    font-size: 15px;
+}
 .online-users {
     flex-wrap: wrap;
     gap: 10px;
+}
+
+h5 {
+    color: var(--primary-color);
+    font-weight: 500;
+    font-size: 15px;
 }
 </style>
