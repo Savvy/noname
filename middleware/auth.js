3168accbb665ca21a90cf2eaf4cb1014082147c2
Malcom Green <malcomeqgreen@gmail.com>
2022-05-04 15:24:46 -0400

    Added logging

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,24 @@
 export default async function ({ route, store, redirect, $axios }) {
   // Check if user is logged in locally.
+  console.log('DISPATCHING');
   if (store.state.auth.isAuthenticated) {
+    console.log('IS AUTHENTICATED');
+    console.log(store.state.auth.user);
     return;
   }
   try {
     // If not logged in, attempt to grab user object.
+    console.log('CHECKING USER')
     let { data } = await $axios.get("/user", { withCredentials: true });
     // Commit change to page
     store.dispatch('auth/setUser', data.user);
-    console.log('DISPATCHING');
 
     // If page is confirmation and the user is verified, redirect to home
     if (route.name === 'confirm-token' && data.user.status === 'Active') {
       redirect('/');
     }
   } catch(error) {
+    console.error(error);
     if (error.response.data.message === 'not_authenticated') {
     // Redirect to login page. User needs to authenticate.
       console.log(error.response.data.message);
